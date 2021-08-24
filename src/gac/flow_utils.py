d92e78b1aa666adbdbf4e39ecacdf54a86878fc2
Walter Mesquita <wapmesquita@users.noreply.github.com>
2021-08-23 22:39:05 -0300

    Restarting from scratch

--- src/gac/flow_utils.py
+++
@@ -1,5 +0,0 @@
-from oauth2client import client
-import constants
-
-def getFlow(redirect_path):
-    return client.flow_from_clientsecrets(constants.KEY, scope=constants.SCOPE, redirect_uri=redirect_path + constants.AUTH_PATH_NAME)
